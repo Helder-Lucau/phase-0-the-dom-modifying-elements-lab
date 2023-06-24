a20810e602f71178a832b737ce1372454c5a7280
Helder <helderlucau@gmail.com>
2023-06-24 21:04:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const main = document.querySelector('#main');
 main.remove();
-//Create element H1
+
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
 newHeader.textContent = "Coding is the champion"
